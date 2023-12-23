bece333dafdc6ec18a1fdadb25021b5d2079f071
ali-zargari <77089778+ali-zargari@users.noreply.github.com>
2023-12-22 22:36:55 -0800

    Fixed background filter bug.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,26 @@
 import "../../styles/App.scss";
-import { useState, useEffect } from "react";
+import {useEffect } from "react";
 import Info from "../input_panel/input_panel.jsx";
 import Result from "../result_panel/result_panel.jsx";
 import Header from "../header/header.jsx";
 import Footer from "../footer/footer.jsx";
 
 function App() {
-	const [count, setCount] = useState(0);
+	//const [count, setCount] = useState(0);
 
 
 
 	useEffect(() => {
-		setCount(count + 1);
-	}, []); // Runs once on mount
+		const adjustHeight = () => {
+			document.body.style.height = window.innerHeight + "px";
+		};
+
+		window.addEventListener("resize", adjustHeight);
+		adjustHeight(); // Initial adjustment
+
+		// Cleanup listener when the component unmounts
+		return () => window.removeEventListener("resize", adjustHeight);
+	}, []);
 
 	return (
 		<>
